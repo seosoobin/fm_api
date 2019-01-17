551e1d7e9c0009d047fd0070bd2a785d2861fcbf
서수빈 <seosoobin@seosubin-ui-MacBook-Pro.local>
2019-01-17 17:02:00 +0900

    production mongodb deploy

--- env/development.js
+++ env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongodb: {
-      // uri: 'mongodb://tiptaptrip-dev:tiptaptrip-dev-password@ds155315.mlab.com:55315/heroku_fq40vmr5'
+     //  uri: 'mongodb://tornadoReal:tornadoReal123@ds137763.mlab.com:37763/heroku_8kzz0kkp'
       uri: 'mongodb://localhost:27017/fm_dev'
     },
     frontServer:'http://localhost:3000'
