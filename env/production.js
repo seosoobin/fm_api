b1374a8f0e541a86c483ab55503a428f68c5f15e
sbseo <sbseo@suholdings.com>
2019-05-27 10:32:42 +0900

    mongodb auth

--- env/production.js
+++ env/production.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongodb: {
-      uri: 'mongodb://tornadoReal:tornadoReal123@ds137763.mlab.com:37763/heroku_8kzz0kkp'
+      uri: 'mongodb://tornadoReal:tornadoReal1@ds137763.mlab.com:37763/heroku_8kzz0kkp'
       //uri: 'mongodb://localhost:27017/fm_dev'
     },
     frontServer:'http://localhost:3000'
