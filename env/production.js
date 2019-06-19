6fad0b2d119f4c924150f4fedc75e3f5fd28cd2d
sbseo <sbseo@suholdings.com>
2019-06-19 13:27:46 +0900

    security for production mode

--- env/production.js
+++ env/production.js
@@ -3,6 +3,7 @@ module.exports = {
       uri: 'mongodb://tornadoReal:tornadoReal1@ds137763.mlab.com:37763/heroku_8kzz0kkp'
       //uri: 'mongodb://localhost:27017/fm_dev'
     },
-    frontServer:'http://localhost:3000'
+    frontServer:'http://localhost:3000',
+    test:"test"
   };
   
