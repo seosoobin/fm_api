784117c92dc73e812345a22bd598c24f02cd4232
sbseo <sbseo@suholdings.com>
2019-06-19 13:34:54 +0900

    fixed untracked files

--- env/production.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mongodb: {
-      uri: 'mongodb://tornadoReal:tornadoReal1@ds137763.mlab.com:37763/heroku_8kzz0kkp'
-      //uri: 'mongodb://localhost:27017/fm_dev'
-    },
-    frontServer:'http://localhost:3000',
-  };
-  
